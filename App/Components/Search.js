7f816cc9d6731eeec17c79ca36f45ca4aa0629d6
Kane Rogers <kane.m.rogers@gmail.com>
2015-10-13 16:26:20 +1100

    Add ListView

--- App/Components/Search.js
+++ App/Components/Search.js
@@ -6,15 +6,38 @@ var {
   TextInput,
   StyleSheet,
   Text,
+  ListView,
   View
 } = React;
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      dataSource: new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2})
+    };
+
+    fetch('http://search-sproutli-bhzq3vdfhs5jhshdoqqt67ru5a.ap-southeast-2.cloudsearch.amazonaws.com/2013-01-01/search?q=-adadad&size=1000')
+      .then((res) => res.json())
+      .then((listings) => {
+        var listings = listings.hits.hit.map((l) => l.fields).filter((l) => l.name && l.name.length > 1);
+        this.setState({
+          dataSource: this.state.dataSource.cloneWithRows(listings)
+        });
+      })
+      .catch((error) => {
+        console.log('Error fetching listings', error);
+      });
+  }
+
   render() {
     return (
       <View style={styles.bigContainer}>
         <SearchBox />
-        <Text>{this.props.preCanned}</Text>
+        <ListView
+          dataSource={this.state.dataSource}
+          renderRow={(rowData, index) => <Text key={index}>{rowData.name}</Text>}
+        />
       </View>
     );
   }
