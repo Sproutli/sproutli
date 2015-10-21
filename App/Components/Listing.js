aec23f48a52ea5146608e6b0d6fa6df9e431b345
Kane Rogers <kane.m.rogers@gmail.com>
2015-10-21 15:34:18 +1100

    Add vegan level, distance and tags.

--- App/Components/Listing.js
+++ App/Components/Listing.js
@@ -12,9 +12,38 @@ var COLOURS = require('../Constants/Colours');
 var VEGAN_LEVELS = require('../Constants/VeganLevels');
 
 class Listing extends React.Component {
-  veganLevel() {
-    if (!this.props.listing.vegan_level) { return ''; }
-    return VEGAN_LEVELS[this.props.listing.vegan_level].short;
+  renderedVeganLevel() {
+    if (!this.props.listing.vegan_level) { return <View />; }
+    return <Text style={styles.subTitle}>{VEGAN_LEVELS[this.props.listing.vegan_level].short}</Text>;
+  }
+
+  renderedLocation() {
+    var distance = this.props.listing.distance;
+    if (!distance) { return <View />; }
+
+    distance = parseFloat(distance).toFixed(1);
+
+    return <Text style={styles.subTitle}>{this.props.listing.locality} ({distance} km)</Text>;
+  }
+
+  renderedRating() {
+    var rating = this.props.listing.rating;
+    if (!rating) { 
+      rating = 'No rating yet'; 
+    } else {
+      rating = `${rating}/5.0`;
+    }
+
+    return <Text style={styles.subTitle}>{rating}</Text>;
+  }
+
+  renderedTags() {
+    var tags = this.props.listing.tags || [];
+    return (
+      <View style={styles.tagContainer}>
+        { tags.map((tag, key) => <Text key={key} style={styles.tags}>#{tag.toLowerCase().replace(' ','')} </Text>) }
+      </View>
+    );
   }
 
   render() {
@@ -22,9 +51,12 @@ class Listing extends React.Component {
       <TouchableHighlight style={styles.card} onPress={this.props.handler}>
         <View>
           <Text style={styles.title}>{this.props.listing.name}</Text>
-          <Text style={styles.subTitle}>{this.veganLevel()}</Text>
-          <Text style={styles.subTitle}>{this.props.listing.distance}</Text>
-          <Text style={styles.tags}>{this.props.listing.tags.join('#')}</Text>
+
+          { this.renderedVeganLevel() }
+          { this.renderedRating() }
+          { this.renderedLocation() }
+
+          { this.renderedTags() }
         </View>
       </TouchableHighlight>
     );
@@ -55,7 +87,11 @@ var styles = StyleSheet.create({
   tags: {
     fontSize: 15,
     fontStyle: 'italic',
-    color: 'white'
+    color: 'white',
+    paddingTop: 20
+  },
+  tagContainer: {
+    flexDirection: 'row'
   }
 });
 
