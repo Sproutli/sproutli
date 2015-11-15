950d07466d3dd082bd0a3dd0c4e02b4751622dc0
Kane Rogers <kane.m.rogers@gmail.com>
2015-11-15 21:41:39 +1100

    Fix up ReviewModal.

--- App/Components/ReviewModal.ios.js
+++ App/Components/ReviewModal.ios.js
@@ -107,7 +107,6 @@ class ReviewModal extends React.Component {
         <Text style={styles.starsText}>{this.state.stars} Stars</Text>
         <TextInput
           style={styles.reviewForm}
-          onSubmitEditing={this._onLeaveReview.bind(this)}
           onChangeText={this._onChangeText.bind(this)}
           value={this.state.reviewText}
           multiline />
