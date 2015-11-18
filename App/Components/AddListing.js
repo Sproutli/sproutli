925d1a4a8440af746bdc0c6f88b55339bc702a7c
Kane Rogers <kane.m.rogers@gmail.com>
2015-11-18 12:37:43 +1100

    Fleshing out forms.

--- App/Components/AddListing.js
+++ App/Components/AddListing.js
@@ -2,18 +2,43 @@
 
 var React = require('react-native');
 var {
-  Text,
   StyleSheet,
-  Platform,
-  View
+  ScrollView
 } = React;
+
+var t = require('tcomb-form-native');
+var Form = t.form.Form;
+
+var CATEGORIES = require('../Constants/Categories');
+var VEGAN_LEVELS = require('../Constants/VeganLevels');
+
+var categoryEnums = {};
+var veganLevelEnums = {};
+
+CATEGORIES.forEach((c) => { categoryEnums[c] = c; });
+VEGAN_LEVELS.forEach((l, i) => { veganLevelEnums[i] = l.short; });
+
+var Category = t.enums(categoryEnums);
+var VeganLevel = t.enums(veganLevelEnums);
+
+
+var Listing = t.struct({
+  name: t.String,
+  categories: Category,
+  description: t.String,
+  tags: t.Array,
+  vegan_level: VeganLevel
+});
 
 class AddListing extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Add your listing!</Text>
-      </View>
+      <ScrollView style={styles.container}>
+        <Form
+          ref='form'
+          type={Listing}
+        />
+      </ScrollView>
     );
   }
 }
@@ -21,7 +46,6 @@ class AddListing extends React.Component {
 var styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginTop: Platform.OS === 'ios' ? 64 : 0,
     padding: 8
   }
 });
