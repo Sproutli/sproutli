ab04f66714fa3dfcfedcea2e50e217dda4d84ad7
Kane Rogers <kane.m.rogers@gmail.com>
2015-11-18 12:23:34 +1100

    Fix up container for AddListing.

--- App/Components/AddListing.js
+++ App/Components/AddListing.js
@@ -3,17 +3,27 @@
 var React = require('react-native');
 var {
   Text,
+  StyleSheet,
+  Platform,
   View
 } = React;
 
 class AddListing extends React.Component {
   render() {
     return (
-      <View style={{flex: 1}}>
+      <View style={styles.container}>
         <Text>Add your listing!</Text>
       </View>
     );
   }
 }
+
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 64 : 0,
+    padding: 8
+  }
+});
 
 module.exports = AddListing;
