78fd09d9cac4d3d5e7c14f5f6687d887fc7a22e4
Kane Rogers <kane.m.rogers@gmail.com>
2016-02-17 21:21:10 +1100

    Move SearchEngine to use location bounds query.
    - Instead of querying for everything and then filtering the response
      locally, we build a "bounding box" around our current location, and
      ask CloudSearch to only return us results in this area.
    - Further optimisations could be made to only return results meeting our
      full search criteria (eg. vegan level, etc.), but this will do the job
      for now.

--- App/Utils/SearchEngine.js
+++ App/Utils/SearchEngine.js
@@ -1,7 +1,52 @@
-/* global fetch */
 'use strict';
 
-var prepareProps = (location) => {
+function getBoundingBox(latlng, distance) {
+  var EARTH_RADIUS_KM = 6371.01, // Earth's radius in km
+      DEG2RAD = Math.PI / 180,
+      RAD2DEG = 180 / Math.PI,
+      MAX_LAT = Math.PI / 2, // 90 degrees
+      MIN_LAT = -MAX_LAT, // -90 degrees
+      MAX_LON = Math.PI, // 180 degrees
+      MIN_LON = -MAX_LON, // -180 degrees
+      FULL_CIRCLE_RAD = Math.PI * 2,
+      inKilometers = true,
+      radius = EARTH_RADIUS_KM;
+
+  var lat = latlng.latitude * DEG2RAD,
+      lon = latlng.longitude * DEG2RAD, 
+      radDist = distance / radius,
+      minLat = lat + radDist,
+      maxLat = lat - radDist,
+      minLon,
+      maxLon,
+      deltaLon;
+
+      console.log('Custom implementation:', radDist, minLat, maxLat);
+  if (minLat > MIN_LAT && maxLat < MAX_LAT) {
+    deltaLon = Math.asin(Math.sin(radDist) / Math.cos(lat));
+    minLon = lon - deltaLon;
+    if (minLon < MIN_LON) {
+      minLon += FULL_CIRCLE_RAD;
+    }
+    maxLon = lon + deltaLon;
+    if (maxLon > MAX_LON) {
+      maxLon -= FULL_CIRCLE_RAD;
+    }
+  } else {
+    // Freakishly, we're in the North or South Pole. Who knew?
+    minLat = Math.max(minLat, MIN_LAT);
+    maxLat = Math.min(maxLat, MAX_LAT);
+    minLon = MIN_LON;
+    maxLon = MAX_LON;
+  }
+
+  return { 
+    se: [minLat * RAD2DEG, minLon * RAD2DEG], 
+    nw: [maxLat * RAD2DEG, maxLon * RAD2DEG ]
+  };
+}
+
+var prepareReturnFields = (location) => {
   var props = [
     'rating',
     'description',
@@ -32,31 +77,55 @@ var prepareProps = (location) => {
 };
 
 var parse = (listings, location) => { 
-  return listings.hits.hit
-  .filter((l) => {
-    if (!location || !location.latitude) { return true; }
+  var response = listings.hits.hit
+    .map((l) => {
+      return({...l.fields, distance: l.exprs ? l.exprs.distance : null});
+    });
 
-    l.fields.distance = l.exprs.distance;
-
-    return l.fields.distance < 20;
-  })
-  .map((l) => l.fields);
+  return response;
 };
 
+var prepareLocationExpression = (location) => {
+  if (!location || !location.latitude) { return ''; }
+
+  return `haversin(${location.latitude},${location.longitude},location.latitude,location.longitude)`;
+};
+
+var prepareFilterQuery = (location) => {
+  if (!location || !location.latitude) { return ''; }
+  const { se, nw } = getBoundingBox(location, 20);
+  return `location:['${se.join(',')}','${nw.join(',')}']`;
+};
+
+var runSearch = (searchParams) => {
+  var { query, parser, returnFields, sort, expr, size, fq } = searchParams;
+  var url;
+
+  if (expr && fq) {
+    url = `http://search-sproutli-bhzq3vdfhs5jhshdoqqt67ru5a.ap-southeast-2.cloudsearch.amazonaws.com/2013-01-01/search?q=${query}&size=${size}&expr.distance=${expr}&return=${returnFields}&sort=${sort}&q.parser=${parser}&fq=${fq}`;
+  } else {
+    url = `http://search-sproutli-bhzq3vdfhs5jhshdoqqt67ru5a.ap-southeast-2.cloudsearch.amazonaws.com/2013-01-01/search?q=${query}&size=${size}&q.parser=${parser}`;
+  }
+
+  return fetch(url);
+};
+
+
+
 var SearchEngine = {
-  prepareLocationQuery(location) {
-    if (!location || !location.latitude) { return ''; }
-
-    return `&expr.distance=haversin(${location.latitude},${location.longitude},location.latitude,location.longitude)&sort=distance asc&return=${prepareProps(location)}`;
-  },
-
   search(query, location) {
     console.log('[SearchEngine] - Searching', query, location);
-    query = query || '-aaoidwjaoiwdjaijwd';
-    var locationQuery = this.prepareLocationQuery(location);
-    var url = `http://search-sproutli-bhzq3vdfhs5jhshdoqqt67ru5a.ap-southeast-2.cloudsearch.amazonaws.com/2013-01-01/search?q=${query}&size=1000${locationQuery}`;
+    const SEARCH_PARAMS = {
+      query: query || 'matchall',
+      parser: 'structured',
+      returnFields: prepareReturnFields(location),
+      sort: 'distance asc',
+      fq: prepareFilterQuery(location),
+      expr: prepareLocationExpression(location),
+      size: 1000,
+    };
 
-    return fetch(url)
+    return runSearch(SEARCH_PARAMS)
       .then((res) => res.json())
       .then((listings) => parse(listings, location))
       .catch((error) => {
