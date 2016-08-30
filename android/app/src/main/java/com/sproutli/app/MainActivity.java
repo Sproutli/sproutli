80a1cf3a1258c58d09ded95109b9f5a701b7cc6a
Kane Rogers <kane.m.rogers@gmail.com>
2016-08-30 19:15:21 +1000

    Update Intercom on Android.

--- android/app/src/main/java/com/sproutli/app/MainActivity.java
+++ android/app/src/main/java/com/sproutli/app/MainActivity.java
@@ -36,15 +36,12 @@ import io.fabric.sdk.android.Fabric;
 import io.intercom.android.sdk.Intercom;
 
 public class MainActivity extends ReactActivity {
-    private final String SENDER_ID = "1034144542573";
-    private String regId; 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fabric.with(this, new Crashlytics(), new Answers());
         Intercom.initialize(getApplication(), "android_sdk-aba6524a84e6953392e1a8583c15970a6dbe851c", "r18lw9fx");
-        Intercom.client().openGCMMessage(getIntent().getData());
+        Intercom.client().openGcmMessage();
     }
 
     /**
